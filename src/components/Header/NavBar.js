c89ea5ef30a6d5d855f35e4485c7dc8d7ec9a354
Steve Dytewski <dy2ski@gmail.com>
2023-01-04 18:44:22 -0800

    added images, broke app with delcaring nav

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -5,14 +5,30 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 // import NavDropdown from 'react-bootstrap/NavDropdown';
 
+export const Navbar = () => {
+const {activeLink, setActiveLink } = useState('home');
+const {scrolled, seSrcolled } = useState(false);
+
+useEffect(() => {
+  const onScroll = () {}
+    if  (window.scrollY > 50) {
+      seSrcolled(true);
+    } else {
+
+      seSrcolled(false);
+
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll)
+}, [])
+
+// function Header() {
 
 
-
-function Header() {
-  const {activeLink, setActiveLink } = useState('home');
-  const {scolled, setScolled } = useState(false);
-  return (
-    <Navbar className="color-nav" expand="lg">
+ return (
+    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
         <Navbar.Brand href="#home">
           <img src={''} alt="Logo" />
@@ -22,9 +38,9 @@ function Header() {
           <span className="navbar-toggler-icon"></span>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projectss</Nav.Link>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link' } onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link' }>Skills</Nav.Link>
+            <Nav.Link href="#projects" <Nav.Link href="#skills" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link' }>Projects</Nav.Link>
 
             <Nav.Link href="portfolio.html">Portfolio</Nav.Link>
             <Nav.Link href="contact.html">Contact</Nav.Link>
