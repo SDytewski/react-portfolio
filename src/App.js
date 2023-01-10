dd5393e01bf42d7ca41dcdcfc3b6c4d8abcf71ec
Steve Dytewski <dy2ski@gmail.com>
2023-01-09 19:06:37 -0800

    removed homescreen, fixed styling for navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import { Container } from 'react-bootstrap'
+// import { Container } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen'
+// import HomeScreen from './screens/HomeScreen'
 
 function App() {
 
   return (
     <div className="App">
       <NavBar />
-        <Container>
-          <HomeScreen />
-        </Container>
+        {/* <Container> */}
+          {/* <HomeScreen /> */}
+        {/* </Container> */}
       <Footer />
     </div>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,22 +1,21 @@
 import { Row, Col } from 'react-bootstrap'
 import Project from '../components/Project'
 import projects from '../projects'
-import './HomeScreen.css'
+// import './HomeScreen.css'
 
-const HomeScreen = () => {
-    return (
-        <>
-            <h1>Latest Projects</h1>
-            <Row className="projectContainer" >
-                {projects.map((project) => (
-                    <Col sm={12} md={6} lg={4} xl={3}>
-                       <Project project={project} hello="hello" goodbye="goodbye" />
-                    </Col>
-                ))}
-            </Row>
-        </>        
+// const HomeScreen = () => {
+//     return (
+//         <>
+//             <Row className="projectContainer pt-5" >
+//                 {projects.map((project) => (
+//                     <Col sm={12} md={6} lg={4} xl={3}>
+//                        <Project project={project} hello="hello" goodbye="goodbye" />
+//                     </Col>
+//                 ))}
+//             </Row>
+//         </>        
                
-     )    
-}
+//      )    
+// }
 
-export default HomeScreen
+// export default HomeScreen
