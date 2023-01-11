d9955d27156010ce0eb1e967088360dae58eec3f
Steve Dytewski <dy2ski@gmail.com>
2023-01-10 20:12:49 -0800

    added header image and got banner page to look decent with header on right

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,16 +1,17 @@
 import {useState, useEffect} from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/img/header.jpg"
+import headerImg from "../assets/img/header8.jpg"
 
 
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = [  "Web Developer", "UI/UX developer", "Front-End Developer" ];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const toRotate = [  "Web Developer", "UI/UX developer", "Front-End Developer" ];
     const period = 2000;
 
     useEffect(() => {
@@ -27,7 +28,26 @@ export const Banner = () => {
         let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
         
         setText(updatedText);
-    }
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+          }
+      
+          if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+          } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+          } else {
+            setIndex(prevIndex => prevIndex + 1);
+          }
+        }
+      
+    
     return (
         <section className="banner" id="home">
             <Container>
@@ -36,11 +56,11 @@ export const Banner = () => {
                         <span className="tagline">
                             Steven Dytewski
                         </span>
-                        <h1>{`Hi! I'm Steve`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "UI/UX developer", "Front-End Developer" ]'><span className="wrap">{text}</span></span></h1>
+                        <h1>{`Hi! I'm Steve `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "UI/UX developer", "Front-End Developer" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
                         </Col>
-                        <Col xs={12} md={6} xl={6}>
+                        <Col xs={12} md={3} xl={3}>
                         <img src={headerImg} alt="Header Img"/>
                         </Col>
                 </Row>
