ab9923dfbd76e3949d752a01ad35bfb0bf75c4dd
Steve Dytewski <dy2ski@gmail.com>
2023-03-10 15:40:24 -0800

    removed space

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,6 @@ import { Navbar, Nav, Container }  from 'react-bootstrap';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
-
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export const NavBar = () => {
