3d8f1b2cc4bd751f519b4964539a9a8bf3327cb0
Steve Dytewski <dy2ski@gmail.com>
2023-06-15 18:59:48 -0700

    adding padding to card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,7 +2,7 @@ import { Col } from "react-bootstrap";
 
 export const ProjectCard = ({ name, text, image, link, git }) => {
   return (
-    <Col size={12} sm={6} md={4}>
+    <Col size={12} sm={6} md={4} className="pb-3">
       <div className="proj-imgbx">
       <a href={link} target="_blank"><img className="proj-pic" src={image} /> </a>
         <div className="proj-txtx pt-3">
