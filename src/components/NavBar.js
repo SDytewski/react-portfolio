7c70a5ed258d8e377b9330c0e811c807b98968cd
Steve Dytewski <dy2ski@gmail.com>
2023-06-22 21:13:56 -0700

    added close navbar hook when user clicks link on hamburger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -33,35 +33,37 @@ const onUpdateActiveLink = (value) => {
   setActiveLink(value);
 }
 
+
+const [expanded, setExpanded] = useState(false);
  return (
-    <Navbar expand="md" className={scrolled ? "scrolled": ""}>
+    <Navbar expanded={expanded} expand="md" className={scrolled ? "scrolled": ""}>
       <Container>
         {/* <Navbar.Brand href="/">
           <img src={''} alt="Logo" />
         
         </Navbar.Brand> */}
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
+        <Navbar.Toggle onClick={() => setExpanded(expanded ? false : "expanded")} aria-controls="basic-navbar-nav">
         <span className="navbar-toggler-icon"></span>
          </Navbar.Toggle> 
         
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link' } onClick={() => onUpdateActiveLink('home')}>HOME</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link' } onClick={() => onUpdateActiveLink('skills')}>SKILLS</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link' } onClick={() => onUpdateActiveLink('projects')}>PROJECTS</Nav.Link>
-            <Nav.Link href="#contact" className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link' } onClick={() => onUpdateActiveLink('contact')}>CONTACT</Nav.Link>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link' } onClick={() => {setExpanded(false); onUpdateActiveLink('home')}}>HOME</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link' } onClick={() => {setExpanded(false); onUpdateActiveLink('skills')}}>SKILLS</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link' } onClick={() => {setExpanded(false); onUpdateActiveLink('projects')}}>PROJECTS</Nav.Link>
+            <Nav.Link href="#contact" className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link' } onClick={() => {setExpanded(false); onUpdateActiveLink('contact')}}>CONTACT</Nav.Link>
             {/* <Nav.Link href="#portfolio.html">Portfolio</Nav.Link> */}
             {/* <Nav.Link href="#contact.html">Contact</Nav.Link> */}
           
-            <Nav.Link href="https://github.com/SDytewski?tab=repositories" target="_blank" className={activeLink === 'GitHub' ? 'active navbar-link' : 'navbar-link' } onClick={() => onUpdateActiveLink('home')}>Github</Nav.Link>
+            <Nav.Link href="https://github.com/SDytewski?tab=repositories" target="_blank" className={activeLink === 'GitHub' ? 'active navbar-link' : 'navbar-link' } onClick={() => {setExpanded(false); onUpdateActiveLink('home')}}>Github</Nav.Link>
             {/* <Nav.Link href="https://www.linkedin.com/in/steven-dytewski/" target="_blank">LinkedIn</Nav.Link> */}
             {/* <Nav.Link href="https://github.com/SDytewski?tab=repositories" target="_blank">Github</Nav.Link> */}
            
           </Nav>
           <span className="navbar-text">
 
-            <div className="social-icon">
-              <a href="https://www.linkedin.com/in/steven-dytewski/" target="_blank"><img src={navIcon1} alt="" /></a>
+            <div className="social-icon" >
+              <a onClick={() => setExpanded(false)} href="https://www.linkedin.com/in/steven-dytewski/" target="_blank"><img src={navIcon1} alt="" /></a>
               {/* <a href="https://www.facebook.com/steve.dytewski/" target="_blank"><img src={navIcon2} alt="" /></a>
               <a href="https://www.instagram.com/stevewave80/" ><img src={navIcon3} alt="" /></a> */}
             </div>
