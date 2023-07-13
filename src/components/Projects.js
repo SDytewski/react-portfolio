c32b0e93a211ba072dbe71a6b25cdce4b0fb5f0c
Steve Dytewski <dy2ski@gmail.com>
2023-07-12 18:13:45 -0700

    added new to do project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,29 +9,30 @@ import TrackVisibility from 'react-on-screen';
 export const Projects = () => {
 
 
-const projects = [
+  const projects = [
 
     {
-  
+
       _id: '1',
-      name: 'MA Taylor',
-      image: '/images/blondehero.jpg',
-      text: 'Wordpress, WooCommerce, JavaScript, CSS',
-      link: "http://mataylorshop.com/",
-      git: "https://github.com/SDytewski/customtheme"
+      name: 'To Do Taskmaker',
+      image: '/images/todopic4.png',
+      text: 'React, Material UI, CSS',
+      link: "https://sdytewski.github.io/task/",
+      git: "https://github.com/SDytewski/task"
     },
-  
+
     {
-  
+
       _id: '2',
       name: 'California Counties',
       image: '/images/california.jpeg',
       text: 'JavaScript, CSS, Wikipedia API',
-      link:"https://sdytewski.github.io/cali/index2.html",
+      link: "https://sdytewski.github.io/cali/index2.html",
       git: "https://github.com/SDytewski/cali"
     },
+
     {
-  
+
       _id: '3',
       name: 'Steve Wave Music',
       image: '/images/stevewave2.png',
@@ -39,44 +40,54 @@ const projects = [
       link: "https://stevewavemusic.com/",
       git: "https://github.com/SDytewski/stevewave"
     },
-  
+
+    {
+
+      _id: '4',
+      name: 'MA Taylor',
+      image: '/images/blondehero2.png',
+      text: 'Wordpress, WooCommerce, JavaScript, CSS',
+      link: "http://mataylorshop.com/",
+      git: "https://github.com/SDytewski/customtheme"
+    },
+
   ];
-  
+
 
   return (
     // <section className="project pt-5 pb-5" id="projects">
-   
-         <section className="project mt-5 pt-5" id="projects">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                <div className="project-bx wow zoomIn">
 
-       
-        <Row>
-          <Col size={12}>
-             <TrackVisibility>
-              {({ isVisible }) =>
-              <div>
-                <h2 className="text-center">Projects</h2>
-                <p  className="text-center">I specialize in React and JavaScript, but I've creatd projects using Ruby on Rails and Wordpress</p>
-               {/* <Tab.Container id="projects-tabs" defaultActiveKey="first">
+    <section className="project mt-5 pt-5" id="projects">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="project-bx wow zoomIn">
+
+
+              <Row>
+                <Col size={12}>
+                  <TrackVisibility>
+                    {({ isVisible }) =>
+                      <div>
+                        <h2 className="text-center">Projects</h2>
+                        <p className="text-center">I specialize in React and JavaScript, but I've created projects using Ruby on Rails and Wordpress</p>
+                        {/* <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   
                   <Tab.Content id="slideInUp" >
                     <Tab.Pane eventKey="first"> */}
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
+                        <Row>
+                          {
+                            projects.map((project, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...project}
                                 />
-                            )
-                          })
-                        }
-                      </Row>
-                    {/* </Tab.Pane>
+                              )
+                            })
+                          }
+                        </Row>
+                        {/* </Tab.Pane>
                     <Tab.Pane eventKey="section">
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
@@ -85,14 +96,14 @@ const projects = [
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container> */}
-              </div>}
-            </TrackVisibility> 
-          </Col>
-        </Row>
-      </div>
-      <img className="background-image-right"></img>
-      </div>
-      </div>
+                      </div>}
+                  </TrackVisibility>
+                </Col>
+              </Row>
+            </div>
+            <img className="background-image-right"></img>
+          </div>
+        </div>
       </div>
     </section>
   )
